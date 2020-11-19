1c3ffbba45c2fd0a19909b0b9dc27b137367568d
SaloEater <danil.lazarev@gmail.com>
2020-11-19 18:57:16 +0300

    Update index.tsx
    Item amount

--- src/component/common/show-recipe/index.tsx
+++ src/component/common/show-recipe/index.tsx
@@ -22,7 +22,7 @@ class InnerShowRecipe extends React.Component<ShowRecipeParams, any>
         const item = this.ItemsStore.getItem(recipe.itemId);
         const stacks = this.props.amount > item.stack ? Math.floor(this.props.amount/item.stack) : 0;
         const stacksString = stacks > 0 ? stacks + " stacks and" : "";
-        const itemsAmount = stacks > 0 ? this.props.amount-stacks*item.stack : item.stack;
+        const itemsAmount = stacks > 0 ? this.props.amount-stacks*item.stack : this.props.amount;
         return <div className={styles.Recipe}>
             {stacksString} {itemsAmount} of {item.name}
             <ShowItemRecipe itemId={item.id} amount={String(this.props.amount)}/>
